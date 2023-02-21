4c309723fc15feefe4c2e2ec997c7363cfeab55e
Hakan ÖZCAN <hozcan@tumsas.local>
2023-02-21 11:41:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+WebRequest SiteyeBaglantiTalebi = HttpWebRequest.Create("https://www.apple.com/tr/");
+WebResponse GelenCevap = SiteyeBaglantiTalebi.GetResponse();
+StreamReader CevapOku = new StreamReader(GelenCevap.GetResponseStream());
+string KaynakKodlar = CevapOku.ReadToEnd();
+//int IcerikBaslangicIndex = KaynakKodlar.IndexOf("<h6>") + 4;
+//int IcerikBitisIndex = KaynakKodlar.Substring(IcerikBaslangicIndex).IndexOf("</h6>");
+//Console.WriteLine(KaynakKodlar.Substring(IcerikBaslangicIndex, IcerikBitisIndex));
+Console.WriteLine(KaynakKodlar);
+Console.Read();
